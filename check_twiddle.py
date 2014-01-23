2025754702614dc674ff7b61b919e323bcc3a630
codylane <clane@rallydev.com>
2014-01-22 19:56:55 -0700

    re-factored code to make it more like a module rather than have global variables all over the place.  Now I need to add tests for the new get_* methods

--- check_twiddle.py
+++ check_twiddle.py
@@ -4,39 +4,92 @@ import urllib2
 DEFAULT_TIMEOUT = 5
 DEFAULT_PORT    = 7001
 
-hostname = ''
-port     = DEFAULT_PORT
-
 # sets the connection timeout value
 socket.setdefaulttimeout(DEFAULT_TIMEOUT)
 
-def build_url(object_name, name, attribute_name):
-  '''
-  Example: If attempting to construct the following:
-  objectName=bean:name=datasource&attributeName=NumBusyConnections
+def build_url(host, object_name, name, attribute_name, port=DEFAULT_PORT):
+    '''
+    Example: If attempting to construct the following:
+    objectName=bean:name=datasource&attributeName=NumBusyConnections
 
-  object_name = 'name'
-  name = 'datasource'
-  attribute_name = 'NumBusyConnections'
+    object_name = 'name'
+    name = 'datasource'
+    attribute_name = 'NumBusyConnections'
 
-  @object_name    The the MBean
-  @name           The name of the Mbean
-  @attribute_name The name of the attribute
-  '''
-  if object_name is None or object_name == '':
-    raise ValueError('object_name cannot be undefined')
-  elif name is None or name == '':
-    raise ValueError('name cannot be undefined')
-  elif attribute_name is None or attribute_name == '':
-    raise ValueError('attribute_name cannot be undefined')
+    @host           The hostname to connect to or ip address
+    @port           The port number to connect to
+    @object_name    The the MBean
+    @name           The name of the Mbean
+    @attribute_name The name of the attribute
+    '''
+    if host is None or host == '':
+        raise ValueError('host cannot be "%s"' %(host))
+    if port is None or port == '' or port <= 0:
+        raise ValueError('port cannot be "%s"' %(port))
+    if object_name is None or object_name == '':
+        raise ValueError('object_name cannot be "%s"' %(object_name))
+    if name is None or name == '':
+        raise ValueError('name cannot be "%s"' %(name))
+    if attribute_name is None or attribute_name == '':
+        raise ValueError('attribute_name cannot be "%s"' %(attribute_name))
+    uri = "http://%s:%s/twiddle/get.op?objectName=%s:name=%s&attributeName=%s" %(host, port, object_name, name, attribute_name)
+    return uri
 
-  uri = "http://%s:%s/twiddle/get.op?objectName=%s:name=%s&attributeName=%s" %(hostname, port, object_name, name, attribute_name)
-  return uri
+def connect(uri):
+    '''
+    Attempts to connect to a host:port by building a pre-canned URI.
+    The request object is returned if the connection is successful.
 
-def connect(object_name, name, attribute_name, host=hostname, host_port=port):
-  global hostname
-  hostname = host
-  global port
-  port = host_port
-  uri = build_url(object_name, name, attribute_name)
-  return urllib2.urlopen(uri)
+    @uri   The URI to connect to, which should be a string like: http://www.google.com
+    @host  Defaults to the global host parameter
+    @port  Defaults to the global port parameter
+
+    @raises URLError if unable to open connection or timeout is reached
+    trying to connect to endpoint.
+    '''
+    return urllib2.urlopen(uri)
+
+def close(request):
+    '''
+    @request A open url request
+    '''
+    request.close()
+
+def get_max_database_connections(host, port=DEFAULT_PORT):
+    uri = build_url(host, 'bean', 'datasource', 'MaxPoolSize', port)
+    req = connect(uri)
+    if req.getcode() == 200:
+        data = req.read()
+        req.close()
+        return data
+
+def get_min_database_connections(host, port=DEFAULT_PORT):
+    uri = build_url(host, 'bean', 'datasource', 'MinPoolSize', port)
+    req = connect(uri)
+    if req.getcode() == 200:
+        data = req.read()
+        req.close()
+        return data
+
+def get_current_used_database_connections(host, port=DEFAULT_PORT):
+    uri = build_url(host, 'bean', 'datasource', 'NumBusyConnections', port)
+    req = connect(uri)
+    if req.getcode() == 200:
+        data = req.read()
+        req.close()
+        return data
+
+def get_current_idle_database_connections(host, port=DEFAULT_PORT):
+    uri = build_url(host, 'bean', 'datasource', 'NumIdleConnections', port)
+    req = connect(uri)
+    if req.getcode() == 200:
+        data = req.read()
+        req.close()
+        return data
+
+if __name__ == '__main__':
+    print("used_connections: %s" %get_current_used_database_connections('somehoest.example.com'))
+    print("idle_connections: %s" %get_current_idle_database_connections('somehoest.example.com'))
+    print("min_connections:  %s" %get_min_database_connections('somehoest.example.com'))
+    print("max_connections:  %s" %get_max_database_connections('somehoest.example.com'))
+
--- tests/Test_Check_Twiddle.py
+++ tests/Test_Check_Twiddle.py
@@ -7,48 +7,12 @@ import urllib2
 def test_DEFAULT_TIMEOUT_is_set():
   assert twiddle.DEFAULT_TIMEOUT is not None or twiddle.DEFAULT_TIMEOUT <= 0, 'Expected that DEFAULT_TIMEOUT be integer > 0, not %s' %(twiddle.DEFAULT_TIMEOUT)
 
-
-def test_hostname_returns_empty_string_when_not_set():
-  expected_value = ''
-  return_val = twiddle.hostname
-  assert return_val == expected_value, 'Expected that twiddle.hostname returns "%s", not %s' %(expected_value, return_val)
-
-def test_hostname_returns_non_empty_string_when_set():
-  return_val = twiddle.hostname
-  assert return_val != '' or return_val is not None, 'Expected that twiddle.hostname to return non-empty string, not %s' %(return_val)
-
-def test_hostname_can_be_set_to_a_string():
-  expected_val = 'foobar'
-  twiddle.hostname = expected_val
-  return_val = twiddle.hostname
-  assert return_val == expected_val, 'Expected that twiddle.host = "%s" be set to %s, not "%s"' %(expected_val, expected_val, return_val)
-
-def test_port_sets_the_default_port_when_no_argument_is_passed():
-  expected_val = twiddle.DEFAULT_PORT
-  return_val = twiddle.port
-  assert return_val == expected_val, 'Expected that twiddle.port be set to default %s, not %s' %(expected_val, return_val)
-
-def test_port_sets_port_to_an_integer():
-  expected_val = 7002
-  twiddle.port = expected_val
-  return_val = twiddle.port
-  assert return_val == expected_val, 'Expected that twiddle.port = %s be set to default %s, not %s' %(expected_val, expected_val, return_val)
-
-
-def test_port_returns_7001_when_using_the_default():
-  expected_val = 7001
-  twiddle.port = twiddle.DEFAULT_PORT
-  return_val = twiddle.port
-  assert return_val == expected_val, 'Expected that twiddle.port be set to default %s, not %s' %(expected_val, return_val)
-
 @parameterized.expand([
   ('somehost.example.com', 7002, 'bean', 'datasource', 'NumBusyConnections'),
 ])
 def test_build_url_returns_non_empty_string(hostname, port, object_name, name, attribute_name):
   expected_val = 'http://%s:%s/twiddle/get.op?objectName=%s:name=%s&attributeName=%s' %(hostname, port, object_name, name, attribute_name)
-  twiddle.hostname = hostname
-  twiddle.port = port
-  return_val = twiddle.build_url(object_name, name, attribute_name)
+  return_val = twiddle.build_url(hostname, object_name, name, attribute_name, port)
   assert return_val == expected_val, 'Expected that twiddle.build_url(%s) be set to "%s", not "%s"' %(expected_val, expected_val, return_val)
 
 @parameterized.expand([
@@ -63,27 +27,44 @@ def test_build_url_returns_non_empty_string(hostname, port, object_name, name, a
 @raises(ValueError)
 def test_build_url_raises_ValueError(hostname, port, object_name, name, attribute_name):
   expected_val = 'http://%s:%s/twiddle/get.op?objectName=%s:name=%s&attributeName=%s' %(hostname, port, object_name, name, attribute_name)
-  twiddle.hostname = hostname
-  twiddle.port = port
-  twiddle.build_url(object_name, name, attribute_name)
+  twiddle.build_url(hostname, object_name, name, attribute_name, port)
 
 @parameterized.expand([
-  ('qd-app-10.blah.prod', '7001', 'bean', 'datasource', 'NumBusyConnections'),
+    ('',),
+    (None,)
 ])
-def test_urlopen(hostname, port, object_name, name, attribute_name):
-  twiddle.hostname = hostname
-  twiddle.port = port
-  uri = twiddle.build_url(object_name, name, attribute_name)
-  req = urllib2.urlopen(uri)
-  response = req.read()
-  print('response=%s, code=%s' %(response, req.getcode()))
-  print(dir(req))
-
+@raises(ValueError)
+def test_build_url_should_raise_ValueError_when_hostname_is_invalid(invalid_hostname):
+    uri = twiddle.build_url(invalid_hostname, 'mybean', 'myname', 'some_attribute', twiddle.DEFAULT_PORT)
 
 @parameterized.expand([
-  ('qd-app-10.blah.prod', '7001', 'bean', 'datasource', 'NumBusyConnections'),
+    ('',),
+    (None,),
+    (0,)
+])
+@raises(ValueError)
+def test_build_url_should_raise_ValueError_when_port_is_invalid(invalid_port):
+    print("invalid_port: '%s'" %(invalid_port))
+    uri = twiddle.build_url('somehoest.example.com', 'mybean', 'myname', 'some_attribute', invalid_port)
+
+############################
+#### integration tests
+############################
+
+@parameterized.expand([
+  ('somehost.example.com', '7001', 'bean', 'datasource', 'NumBusyConnections'),
 ])
 def test_connect(hostname, port, object_name, name, attribute_name):
-  req = twiddle.connect(object_name, name, attribute_name, hostname, port)
+  uri = twiddle.build_url(hostname, object_name, name, attribute_name, port)
+  req = twiddle.connect(uri)
   assert req is not None
   assert req.getcode() == 200
+
+def test_close_closes_down_open_pointer():
+    hostname = 'somehost.example.com'
+    port = '7001'
+    uri = twiddle.build_url(hostname, 'bean', 'datasource', 'NumBusyConnection', port)
+    req = twiddle.connect(uri)
+    twiddle.close(req)
+    assert req.fp is None
+
