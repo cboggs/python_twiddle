0f774e152c9eca89735b71f4e02c48a17192b922
codylane <clane@rallydev.com>
2014-01-24 18:21:53 -0700

    fixing hostname in integration tests

--- tests/Test_check_almconnections.py
+++ tests/Test_check_almconnections.py
@@ -22,7 +22,7 @@ def test_get_database_connection_maximum_raises_ValueError_when_arguments_are_in
     almconnections.get_database_connection_maximum(invalid_hostname, invalid_port)
 
 @parameterized.expand([
-    ('qd-app-01.rally.prod', '7001'),
+    ('mytesthost.com', '7001'),
 ])
 def test_get_database_connection_maximum_returns_a_non_empty_string(host, port):
     return_val = almconnections.get_database_connection_maximum(host, port)
@@ -48,7 +48,7 @@ def test_get_database_connection_minimum_raises_ValueError_when_arguments_are_in
     almconnections.get_database_connection_minimum(invalid_hostname, invalid_port)
 
 @parameterized.expand([
-    ('qd-app-01.rally.prod', '7001'),
+    ('mytesthost.com', '7001'),
 ])
 def test_get_database_connection_minimum_returns_a_non_empty_string(host, port):
     return_val = almconnections.get_database_connection_minimum(host, port)
@@ -75,7 +75,7 @@ def test_get_database_connection_current_used_raises_ValueError_when_arguments_a
     almconnections.get_database_connection_current_used(invalid_hostname, invalid_port)
 
 @parameterized.expand([
-    ('qd-app-01.rally.prod', '7001'),
+    ('mytesthost.com', '7001'),
 ])
 def test_get_database_connection_current_used_returns_a_non_empty_string(host, port):
     return_val = almconnections.get_database_connection_current_used(host, port)
@@ -101,7 +101,7 @@ def test_get_database_connection_current_idle_raises_ValueError_when_arguments_a
     almconnections.get_database_connection_current_idle(invalid_hostname, invalid_port)
 
 @parameterized.expand([
-    ('qd-app-01.rally.prod', '7001'),
+    ('mytesthost.com', '7001'),
 ])
 def test_get_database_connection_current_idle_returns_a_non_empty_string(host, port):
     return_val = almconnections.get_database_connection_current_idle(host, port)
