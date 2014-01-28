1b7cd765d6af9524ea7b2fbe79d81bfd63d4c74e
codylane <clane@rallydev.com>
2014-01-27 18:50:43 -0700

    subproject commit

--- __init__.py
+++ __init__.py
@@ -1,10 +1,6 @@
 import twiddle.check_twiddle
 import twiddle.check_almconnections
-
-import sys
-import os
-
+from twiddle.daemon import daemon
 from twiddle.signalhandler import SignalHandler
 import twiddle.tests as tests
 
-import twiddle.daemon
