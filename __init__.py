496709553c477c6dacb312ac51b790a8d174548a
codylane <clane@rallydev.com>
2014-01-27 19:01:03 -0700

    fixes problems with __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 import twiddle.check_twiddle
 import twiddle.check_almconnections
-from twiddle.daemon import daemon
+from twiddle.daemon.daemon import Daemon
 from twiddle.signalhandler import SignalHandler
 import twiddle.tests as tests
 
