ec6dd28e01aaa5c08d9b8c970adcb32523590585
codylane <clane@rallydev.com>
2014-01-30 12:04:50 -0700

    fixing __init__.py wiht correct name spaces

--- __init__.py
+++ __init__.py
@@ -1,6 +1,4 @@
-import twiddle.twiddle
-import twiddle.check_almconnections
-from twiddle.daemon.daemon import Daemon
-from twiddle.signalhandler import SignalHandler
-import twiddle.tests as tests
+from daemon.daemon import Daemon
+from signalhandler import SignalHandler
+import tests as tests
 
